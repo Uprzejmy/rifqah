3765b486683755cff0ebda4d4db415f0232a8535
Łukasz Merynda <lmerynda@gmail.com>
2015-07-06 07:04:03 +0200

    cleanup

--- parameters.php
+++ parameters.php
@@ -1,18 +1,13 @@
 <?php
 
-  // $dbHost = "mysql.agh.edu.pl";//mysql.agh.edu.pl
-  // $dbName = "rifqahDB";
-  // $dbUser = "aljubour";//aljubour
-  // $dbPassword = "Xtmvm2qw";//Xtmvm2qw
+  // $dbHost = "mysql.agh.edu.pl";
+  // $dbName = "aljubour";
+  // $dbUser = "aljubour";
+  // $dbPassword = "Xtmvm2qw";
 
-  //pw: Rulahuz
-
-  $dbHost = "localhost";//mysql.agh.edu.pl
+  $dbHost = "localhost";
   $dbName = "rifqahDB";
-  $dbUser = "rifqah";//aljubour
-  $dbPassword = "rifqah1234";//Xtmvm2qw
-  $root = "/var/www/rifqah/";
-  $adminLogin = "admin";
-  $adminPassword = "admin1234";
+  $dbUser = "rifqah";
+  $dbPassword = "rifqah1234";
 
 ?>
